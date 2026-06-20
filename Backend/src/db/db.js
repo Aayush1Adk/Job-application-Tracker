e1e7540372918d5a545897505309203620e60c44
Aayush1Adk <aayush.adhikari2076@gmail.com>
2026-06-20 13:29:50 +0545

    Debug database config

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const {Pool} = require("pg");
 
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_NAME =", process.env.DB_NAME);
 
 const pool = new Pool({
     user: process.env.DB_USER,
